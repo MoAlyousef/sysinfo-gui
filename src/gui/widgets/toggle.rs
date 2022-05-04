c5ad171274a8922b8cccae1094a7a23b9f383ad3
MoAlyousef <may642_2000@hotmail.com>
2022-05-04 18:15:25 +0300

    fix toggle alignment

--- src/gui/widgets/toggle.rs
+++ src/gui/widgets/toggle.rs
@@ -15,7 +15,7 @@ impl Default for Toggle {
 
 impl Toggle {
     pub fn new(x: i32, y: i32, w: i32, h: i32, label: &str) -> Self {
-        let p = group::Pack::new(x, y, w, h, None).with_label(label);
+        let p = group::Pack::new(x, y, w, h, None).with_label(label).with_align(Align::Left);
         frame::Frame::default().with_size(w, 7);
         let mut btn = button::ToggleButton::new(x, y, w, 14, "@+6square")
             .with_align(Align::Left | Align::Inside);
@@ -77,7 +77,7 @@ impl Default for RoundToggle {
 
 impl RoundToggle {
     pub fn new(x: i32, y: i32, w: i32, h: i32, label: &str) -> Self {
-        let p = group::Pack::new(x, y, w, h, None).with_label(label);
+        let p = group::Pack::new(x, y, w, h, None).with_label(label).with_align(Align::Left);
         let mut btn = button::ToggleButton::new(x, y, w, 30, "@+6circle")
             .with_align(Align::Left | Align::Inside);
         btn.set_frame(FrameType::RFlatBox);
